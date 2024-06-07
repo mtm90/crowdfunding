f199ec2c167482a033e7b37256ac4572d221df90
mtm90 <mattia22nchi@gmail.com>
2024-06-07 09:46:22 +0200

    new build

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from './components/HomePage';
+import BlogPage from './components/BlogPage';
+import BlogPostPage from './components/BlogPostPage';
+import AboutUsPage from './components/AboutUsPage';
+import DonatePage from './components/DonatePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogPostPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/donate" element={<DonatePage />} />
+      </Routes>
+      <Footer />
+    </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
